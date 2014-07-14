ee402d8524e9828e2d872343e9fca13c40b7b0b1
Blake R. Johnson <bjohnson@bbn.com>
2014-07-14 14:03:43 -0400

    Consistent indendentation. Fixes #6.

--- src/curve_fit.jl
+++ src/curve_fit.jl
@@ -1,10 +1,10 @@
 
 immutable LsqFitResult{T}
-    # simple type container for now, but can be expanded later
-    dof::Int
-    param::Vector{T}
-    resid::Vector{T}
-    jacobian::Matrix{T}
+	# simple type container for now, but can be expanded later
+	dof::Int
+	param::Vector{T}
+	resid::Vector{T}
+	jacobian::Matrix{T}
 end
 
 function lmfit(f::Function, p0; kwargs...)
@@ -28,8 +28,8 @@ function lmfit(f::Function, p0; kwargs...)
 
 	results = Optim.levenberg_marquardt(f, g, p0; kwargs...)
 	p = results.minimum
-    resid = f(p)
-    dof = length(resid) - length(p)
+	resid = f(p)
+	dof = length(resid) - length(p)
 	return LsqFitResult(dof, p, f(p), g(p))
 end
 
@@ -47,13 +47,13 @@ function curve_fit(model::Function, xpts, ydata, wt::Vector, p0; kwargs...)
 end
 
 function curve_fit(model::Function, xpts, ydata, wt::Matrix, p0; kwargs...)
-    # as before, construct a weighted cost function with where this
-    # method uses a matrix weight.
-    # for example: an inverse_covariance matrix
+	# as before, construct a weighted cost function with where this
+	# method uses a matrix weight.
+	# for example: an inverse_covariance matrix
 
 	# Cholesky is effectively a sqrt of a matrix, which is what we want
 	# to minimize in the least-squares of levenberg_marquardt()
-    # This requires the matrix to be positive definite
+	# This requires the matrix to be positive definite
 	u = chol(wt)
 
 	f(p) = u * ( model(xpts, p) - ydata )
@@ -61,9 +61,9 @@ function curve_fit(model::Function, xpts, ydata, wt::Matrix, p0; kwargs...)
 end
 
 function estimate_covar(fit::LsqFitResult)
-    # computes covariance matrix of fit parameters
-    r = fit.resid
-    J = fit.jacobian
+	# computes covariance matrix of fit parameters
+	r = fit.resid
+	J = fit.jacobian
 
 	# mean square error is: standard sum square error / degrees of freedom
 	mse = Optim.sse(r) / fit.dof
@@ -75,10 +75,10 @@ function estimate_covar(fit::LsqFitResult)
 end
 
 function estimate_errors(fit::LsqFitResult, alpha=0.95)
-    # computes (1-alpha) error estimates from
-    #   fit   : a LsqFitResult from a curve_fit()
+	# computes (1-alpha) error estimates from
+	#   fit   : a LsqFitResult from a curve_fit()
 	#   alpha : alpha percent confidence interval, (e.g. alpha=0.95 for 95% CI)
-    covar = estimate_covar(fit)
+	covar = estimate_covar(fit)
 
 	# then the standard errors are given by the sqrt of the diagonal
 	std_error = sqrt(diag(covar))
