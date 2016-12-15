e22828c195a8e7d90c58276446c58596bcd116e3
Blake R. Johnson <blake.johnson@raytheon.com>
2016-12-15 17:00:01 -0500

    Put curve_fit tests in a let block to avoid model redefinition warnings.

--- test/curve_fit.jl
+++ test/curve_fit.jl
@@ -1,14 +1,17 @@
-# fitting noisy data to an exponential model
-model(x, p) = p[1]*exp(-x.*p[2])
+let
+    # fitting noisy data to an exponential model
+    model(x, p) = p[1]*exp(-x.*p[2])
 
-# some example data
-srand(12345)
-xdata = linspace(0,10,20)
-ydata = model(xdata, [1.0 2.0]) + 0.01*randn(length(xdata))
+    # some example data
+    srand(12345)
+    xdata = linspace(0,10,20)
+    ydata = model(xdata, [1.0, 2.0]) + 0.01*randn(length(xdata))
 
-fit = curve_fit(model, xdata, ydata, [0.5, 0.5])
-@assert norm(fit.param - [1.0, 2.0]) < 0.05
+    fit = curve_fit(model, xdata, ydata, [0.5, 0.5])
+    @assert norm(fit.param - [1.0, 2.0]) < 0.05
+    @test fit.converged
 
-# can also get error estimates on the fit parameters
-errors = estimate_errors(fit)
-@assert norm(errors - [0.017, 0.075]) < 0.01
+    # can also get error estimates on the fit parameters
+    errors = estimate_errors(fit)
+    @assert norm(errors - [0.017, 0.075]) < 0.01
+end
