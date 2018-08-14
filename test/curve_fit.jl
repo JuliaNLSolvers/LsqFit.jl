0dc1abdd9a160ca33dc74902fe128fefadb45e82
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-08-14 12:05:51 +0200

    Update curve_fit.jl

--- test/curve_fit.jl
+++ test/curve_fit.jl
@@ -4,7 +4,7 @@ let
     model(x, p) = p[1] .* exp.(-x .* p[2])
 
     # some example data
-    srand(12345)
+    Random.seed!(12345)
     xdata = range(0, stop=10, length=20)
     ydata = model(xdata, [1.0, 2.0]) + 0.01*randn(length(xdata))
     p0 = [0.5, 0.5]
