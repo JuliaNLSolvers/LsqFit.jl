7a29b973567a6e49e081f6c8e747e7f895a5a2c7
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2022-09-03 13:14:40 +0200

    Clean up the whole project.

--- src/LsqFit.jl
+++ src/LsqFit.jl
@@ -1,27 +1,80 @@
 module LsqFit
 
-    export curve_fit,
-           margin_error,
-           confidence_interval,
-           estimate_covar,
-           make_hessian,
-           Avv,
-           # StatsBase reexports
-           dof, coef, nobs, mse, rss,
-           stderror, weights, residuals
+export curve_fit,
+    margin_error,
+    confidence_interval,
+    estimate_covar,
+    make_hessian,
+    Avv,
+    # StatsBase reexports
+    dof,
+    coef,
+    nobs,
+    mse,
+    rss,
+    stderror,
+    weights,
+    residuals
 
-    using Distributions
-    using OptimBase
-    using LinearAlgebra
-    using ForwardDiff
-    import NLSolversBase: value, jacobian
-    import StatsBase
-    import StatsBase: coef, dof, nobs, rss, stderror, weights, residuals
+using Distributions
+using LinearAlgebra
+using ForwardDiff
+using Printf
 
-    import Base.summary
+import NLSolversBase:
+    value, value!, jacobian, jacobian!, value_jacobian!!, OnceDifferentiable
+import StatsBase
+import StatsBase: coef, dof, nobs, rss, stderror, weights, residuals
 
-    include("geodesic.jl")
-    include("levenberg_marquardt.jl")
-    include("curve_fit.jl")
+import Base.summary
+
+
+struct LsqFitState{T}
+    iteration::Int
+    value::Float64
+    g_norm::Float64
+    metadata::Dict
+end
+function Base.show(io::IO, t::LsqFitState)
+    @printf io "%6d   %14e   %14e\n" t.iteration t.value t.g_norm
+    if !isempty(t.metadata)
+        for (key, value) in t.metadata
+            @printf io " * %s: %s\n" key value
+        end
+    end
+    return
+end
+
+LsqFitTrace{T} = Vector{LsqFitState{T}}
+function Base.show(io::IO, tr::LsqFitTrace)
+    @printf io "Iter     Function value   Gradient norm \n"
+    @printf io "------   --------------   --------------\n"
+    for state in tr
+        show(io, state)
+    end
+    return
+end
+
+struct LsqFitResults{O,T,N}
+    method::O
+    initial_x::Array{T,N}
+    minimizer::Array{T,N}
+    minimum::T
+    iterations::Int
+    iteration_converged::Bool
+    x_converged::Bool
+    g_converged::Bool
+    g_tol::Float64
+    trace::LsqFitTrace{O}
+    f_calls::Int
+    g_calls::Int
+end
+
+minimizer(lsr::LsqFitResults) = lsr.minimizer
+isconverged(lsr::LsqFitResults) = lsr.x_converged || lsr.g_converged
+
+include("geodesic.jl")
+include("levenberg_marquardt.jl")
+include("curve_fit.jl")
 
 end
--- src/geodesic.jl
+++ src/geodesic.jl
@@ -6,58 +6,63 @@ end
 
 struct OutOfPlace{F,D}
     f!::F
-    out_of_place_funcs::Dict{Type, D}
+    out_of_place_funcs::Dict{Type,D}
     make_buffer::D
-    
-    function OutOfPlace(f!::T, dict::Dict{Type, D}, shape::Tuple) where {T,D}
-        
+
+    function OutOfPlace(f!::T, dict::Dict{Type,D}, shape::Tuple) where {T,D}
+
         mk(::T, p) = similar(p, shape)
         new{T,D}(f!, dict, mk)
     end
 end
 
-OutOfPlace(f!, shape) = OutOfPlace(f!, Dict{Type, Function}(), shape) #no sure how to remove the `Function` here
+OutOfPlace(f!, shape) = OutOfPlace(f!, Dict{Type,Function}(), shape) #no sure how to remove the `Function` here
 
 eval_f(f::F, x) where {F} = f(x) # function barrier
 function (oop::OutOfPlace{F})(x) where {F}
     T = eltype(x)
-    f = get!(() -> make_out_of_place_func(oop.f!, x, oop.make_buffer), oop.out_of_place_funcs, T)
+    f = get!(
+        () -> make_out_of_place_func(oop.f!, x, oop.make_buffer),
+        oop.out_of_place_funcs,
+        T,
+    )
     eval_f(f, x)
 end
 
 
 
-function make_hessian(f!,x0,p0)
-    
+function make_hessian(f!, x0, p0)
+
     f = OutOfPlace(f!, (length(x0),))
     g! = (outjac, p) -> (ForwardDiff.jacobian!(outjac, f, p); outjac = outjac')
-    g = OutOfPlace(g!,(length(x0),length(p0)))
-    h! = (outjac2, p) -> (ForwardDiff.jacobian!(outjac2, g, p); reshape(outjac2',length(p0),length(p0),length(x0)))
+    g = OutOfPlace(g!, (length(x0), length(p0)))
+    h! = (outjac2, p) -> (ForwardDiff.jacobian!(outjac2, g, p);
+    reshape(outjac2', length(p0), length(p0), length(x0)))
     h!
 end
 
 struct Avv
-	h!::Function
+    h!::Function
     hessians::Array{Float64}
-    
+
     function Avv(h!::Function, n::Int, m::Int)
-    	hessians = Array{Float64}(undef, m*n, n)
+        hessians = Array{Float64}(undef, m * n, n)
         new(h!, hessians)
     end
 end
 
 function (avv::Avv)(dir_deriv::AbstractVector, p::AbstractVector, v::AbstractVector)
     hess = avv.h!(avv.hessians, p) #half of the runtime
-    vHv!(dir_deriv, hess,v) #half of the runtime, almost all the memory
+    vHv!(dir_deriv, hess, v) #half of the runtime, almost all the memory
 end
 
-function vHv!(dir_deriv::AbstractVector, hessians::AbstractArray,v::AbstractVector)
+function vHv!(dir_deriv::AbstractVector, hessians::AbstractArray, v::AbstractVector)
     tmp = similar(v) #v shouldn't be too large in general, so I kept it here
     vt = v'
-    
-    for i=1:length(dir_deriv)
-        @views mul!(tmp, hessians[:,:,i], v) #this line is particularly expensive memory-wise
-        dir_deriv[i] = vt*tmp
+
+    for i = 1:length(dir_deriv)
+        @views mul!(tmp, hessians[:, :, i], v) #this line is particularly expensive memory-wise
+        dir_deriv[i] = vt * tmp
     end
-    
+
 end
