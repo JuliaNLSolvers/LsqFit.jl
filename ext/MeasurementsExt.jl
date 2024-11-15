ec47d05cc5f23780a361404ce19001d932f6d078
chriswaudby <cwaudby@gmail.com>
2024-11-15 10:53:13 +0000

    Add method for jacobian

--- ext/MeasurementsExt.jl
+++ ext/MeasurementsExt.jl
@@ -17,4 +17,19 @@ function LsqFit.curve_fit(
     curve_fit(model, xdata, y, wt, p0; inplace=inplace, kwargs...)
 end
 
+function LsqFit.curve_fit(
+    model,
+    jacobian,
+    xdata::AbstractArray,
+    ydata::AbstractArray{Measurement{T}} where T,
+    p0::AbstractArray;
+    inplace=false,
+    kwargs...,
+)
+    y = Measurements.value.(ydata)
+    ye = Measurements.uncertainty.(ydata)
+    wt = ye .^ -2
+    curve_fit(model, jacobian, xdata, y, wt, p0; inplace=inplace, kwargs...)
+end
+
 end # module
