52977c4499800010986a92260a307f5bbb2b9599
JamesWrigley <james@puiterwijk.org>
2025-04-06 13:28:18 +0200

    Add a precompilation workload

--- src/LsqFit.jl
+++ src/LsqFit.jl
@@ -32,4 +32,26 @@ include("geodesic.jl")
 include("levenberg_marquardt.jl")
 include("curve_fit.jl")
 
+import PrecompileTools: @compile_workload
+
+@compile_workload begin
+    model(x, p) = @. p[1] * x + p[2]
+
+    local fit
+    for T in (Float32, Float64)
+        xdata = T.(1:100)
+        ydata = model(xdata, T[1.0, 1.0])
+        p0 = T[0.8, 0.8]
+
+        for ad in (:finite, :finiteforward, :forwarddiff)
+            fit = curve_fit(model, xdata, ydata, p0; autodiff=ad)
+        end
+    end
+
+    stderror(fit)
+    margin_error(fit)
+    confint(fit)
+    vcov(fit)
+end
+
 end
